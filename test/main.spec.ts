b4f8e9a728011145f77b96fd14df877076e94c77
raiper34 <raipergm34@gmail.com>
2022-08-06 19:39:26 +0200

    test(unit-tests): add missing unit tests to cover 100%

--- test/main.spec.ts
+++ test/main.spec.ts
@@ -1,5 +1,6 @@
 import {M3uParser} from "../src/m3u-parser";
 import {complex, extGroupDirectiveOrder} from "./test-m3u";
+import {M3uMedia, M3uPlaylist} from "../dist";
 
 describe('Parse and generate test', () => {
     it('should be same as original after parse and generate', () => {
@@ -10,5 +11,16 @@ describe('Parse and generate test', () => {
         const parsed = M3uParser.parse(extGroupDirectiveOrder);
         expect(parsed.medias[0].group).toEqual('Test TV group 1');
         expect(parsed.medias[1].group).toEqual('Test TV group 2');
+    });
+
+    it('should raise exception when first line is missing', () => {
+        const stringWithoutFirstLine = extGroupDirectiveOrder.split('\n').slice(1).join('\n');
+        expect(() => M3uParser.parse(stringWithoutFirstLine)).toThrow(new Error('Missing #EXTM3U directive!'));
+    });
+
+    it('should generate without playlist title', () => {
+        const playlist = new M3uPlaylist();
+        playlist.medias.push(new M3uMedia('location'));
+        expect(playlist.getM3uString()).toEqual('#EXTM3U\nlocation');
     });
 });
