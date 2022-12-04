db1904144826e6aec2381d807069e7609cb91937
Alexander Koshman <akoshman777+github@gmail.com>
2022-12-05 00:31:34 +0200

    fix: ignore blank lines

--- src/m3u-parser.ts
+++ src/m3u-parser.ts
@@ -115,7 +115,8 @@ export class M3uParser {
 
   /**
    * Parse is static method to parse m3u playlist string into m3u playlist object.
-   * Playlist need to contains #EXTM3U directive on first line.
+   * Playlist need to contain #EXTM3U directive on first line.
+   * All lines are trimmed and blank ones are removed.
    * @param m3uString - whole m3u playlist string
    * @returns parsed m3u playlist object
    * @example
@@ -125,7 +126,12 @@ export class M3uParser {
    * ```
    */
   static parse(m3uString: string): M3uPlaylist {
-    const lines = m3uString.split('\n').filter(item => item);
+    if (!m3uString) {
+      throw new Error(`m3uString can't be null!`);
+    }
+
+    const lines = m3uString.split('\n').map(item => item.trim()).filter(item => item != '');
+
     if (!this.isValidM3u(lines)) {
       throw new Error(`Missing ${M3uDirectives.EXTM3U} directive!`);
     }
