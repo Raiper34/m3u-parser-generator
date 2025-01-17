437bafb4da9b4284dc6a5343dae4cbe827ad814d
raiper34 <raipergm34@gmail.com>
2025-01-17 21:35:53 +0100

    chore(test): add vitestui for better unit test experience

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,9 +3,10 @@ import {defineConfig} from 'vitest/config'
 export default defineConfig({
     plugins: [],
     test: {
+        include: ["test/**/*.spec.ts"],
         coverage: {
             include: ["src/**"],
-            reporter: ['lcov']
+            reporter: ['html', 'lcov']
         },
     },
 });
